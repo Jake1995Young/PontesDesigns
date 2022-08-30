197bf2374555c2feee5f9863196fc41993b32c2e
Jake Young <jake_young@dell.com>
2022-08-30 23:18:55 +0100

    Adding base structure

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, {useState}  from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+    return(
+        <div className='pd__navbar section__padding' id='home'>
+            Navbar
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/containers/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './header.css'
+import logo from '../../assets/rp-logo.svg'
+
+const Header = () => {
+    return (
+        <div className='pd__header section__padding' id='home'>
+            Header
+        </div>
+    )
+}
+
+export default Header
